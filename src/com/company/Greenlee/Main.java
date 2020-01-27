88ab8baf0c6e56ddb06fcfefe50a3389744abedf
trentonjavadroid1660 <59889828+trentonjavadroid1660@users.noreply.github.com>
2020-01-26 20:26:52 -0500

    Update Main.java

--- src/com/company/Greenlee/Main.java
+++ src/com/company/Greenlee/Main.java
@@ -10,34 +10,28 @@ public class Main {
     // Importing Scanner input to be able to use user inputs for all data types.
     private static Scanner input = new Scanner(System.in);
 
-    // Created a Variables class to have both variables act global within the program.
-    private static class Variables {
-        private static double fahrenheit;
-        private static double celsius;
-    }
-
     // Methods.
-    // User inputting degrees fahrenheit.
-    private static void userInput() {
-        System.out.println("Enter a temperature in fahrenheit (Enter '-461' or lower to end the program): ");
-        Variables.fahrenheit = input.nextDouble();
-    }
-
     // Calculator to determine degrees fahrenheit into degrees celsius.
-    private static void celsiusCalculator() {
-        Variables.celsius = (Variables.fahrenheit - 32) / 1.8;
+    static double celsiusCalculator(double fahrenheit) {
+        double celsius = (fahrenheit - 32) / 1.8;
+        return celsius;
     }
 
     // Output to print out degrees celsius to the user.
     // Importing DecimalFormat class to restrict the value of Variables.celsius to only two decimal places.
-    private static void output() {
+    private static void output(double celsiusValue) {
         DecimalFormat celsiusFormat = new DecimalFormat("###.##");
-        System.out.println("This temperature in Celsius is: " + celsiusFormat.format(Variables.celsius) + "C°");
+        double celsiusCalculation = (celsiusCalculator(celsiusValue));
+        System.out.print("This temperature in Celsius is: " + celsiusFormat.format(celsiusCalculation) + " C°\n");
     }
 
     public static void main(String[] args) {
-        // Calling userInput() so the while loop can read the users inputted value for fahrenheit
-        userInput();
+        /* Write a program that prompts a user to enter a temperature in Fahrenheit, converts it to Celsius,
+        and displays the Celsius temperature. Separate the code that collects user input, the code that converts
+        temperatures, and the code that displays the output into different methods. The program should repeat until
+        the user enters a value less than -460 (approximately absolute zero).*/
+        System.out.println("Enter a temperature in fahrenheit(Enter '-461' or lower to end the program): ");
+        double fahrenheitInput = input.nextDouble();
 
         // Using a while loop to determine if the userInput() value is greater than or equal to -460.
         // Will not execute if 'less than -460'.
@@ -45,10 +39,10 @@ public class Main {
         // Calling celsiusCalculator() to calculate userInput() value into degrees celsius.
         // Calling output() to display the results from celsiusCalculator().
         // Calling upon userInput() again since the previous userInput value was true
-        while (Variables.fahrenheit >= -460) {
-            celsiusCalculator();
-            output();
-            userInput();
+        while (fahrenheitInput >= -460) {
+            output(fahrenheitInput);
+            System.out.println("Enter a temperature in fahrenheit(Enter '-461' or lower to end the program): ");
+            fahrenheitInput = input.nextDouble();
         }
     }
 }
