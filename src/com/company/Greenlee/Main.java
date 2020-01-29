f596dfc5bc4b70eda69dc364a1d7930ec30229ee
trentonjavadroid1660 <59889828+trentonjavadroid1660@users.noreply.github.com>
2020-01-28 22:34:27 -0500

    Update Main.java

--- src/com/company/Greenlee/Main.java
+++ src/com/company/Greenlee/Main.java
@@ -11,15 +11,16 @@ public class Main {
 
     public static void main(String[] args) {
         double value = 0;
-        input(value);
-        while (value != -461) {
-            output(value);
-        }
+        value = output(value);
+        
         // Using a while loop to determine if the userInput() value is greater than or equal to -460.
         // Will not execute if 'less than -460'.
         // Will execute if greater than or equal to -460.
         // Calling output() to display the results from celsiusCalculator().
         // Calling upon userInput() again since the previous userInput value was true
+        while (value >= -460) {
+            output(value);
+        }
 
     }
 
@@ -27,6 +28,12 @@ public class Main {
     public static double input(double fahrenheitInput) {
         System.out.println("Enter a temperature in fahrenheit(Enter '-461' or lower to end the program): ");
         fahrenheitInput = input.nextDouble();
+        if (fahrenheitInput >= -460) {
+            return fahrenheitInput;
+        } else {
+            System.out.println("Exiting Program");
+            System.exit(0);
+        }
         return fahrenheitInput;
     }
 
@@ -34,15 +41,17 @@ public class Main {
     // Calculator to determine degrees fahrenheit into degrees celsius.
     public static double celsiusCalculator(double fahrenheit) {
         double fah = input(fahrenheit);
-        double celsius = (fah - 32) / 1.8;
+        double celsius = 0;
+        celsius = (fah - 32.0) / 1.8;
         return celsius;
     }
 
     // Output to print out degrees celsius to the user.
     // Using DecimalFormat class to restrict the celsius value to only two decimal places.
-    public static void output(double celsiusValue) {
+    public static double output(double celsiusValue) {
         DecimalFormat celsiusFormat = new DecimalFormat("###.##");
         double celsiusCalculation = (celsiusCalculator(celsiusValue));
         System.out.print("This temperature in Celsius is: " + celsiusFormat.format(celsiusCalculation) + " C°\n\n");
+        return celsiusCalculation;
     }
 }
